7559789fbdab3aa8cd425bfd4e5ff6856b8e55fc
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-01-15 17:49:00 +0800

    removed create button. When user move part to quarantine, auto populate the necessary data

--- backend/controllers/ScrapController.php
+++ backend/controllers/ScrapController.php
@@ -13,6 +13,7 @@ use yii\filters\AccessControl;
 use yii\helpers\ArrayHelper;
 use common\models\UserGroup;
 use common\models\UserPermission;
+use common\models\WorkOrderPart;
 
 /**
  * ScrapController implements the CRUD actions for Scrap model.
@@ -25,8 +26,8 @@ class ScrapController extends Controller
     public function behaviors()
     {
         $userGroupArray = ArrayHelper::map(UserGroup::find()->all(), 'id', 'name');
-       
-        foreach ( $userGroupArray as $uGId => $uGName ){ 
+
+        foreach ( $userGroupArray as $uGId => $uGName ){
             $permission = UserPermission::find()->where(['controller' => 'Scrap'])->andWhere(['user_group_id' => $uGId ] )->all();
             $actionArray = [];
             foreach ( $permission as $p )  {
@@ -39,7 +40,7 @@ class ScrapController extends Controller
                 $allow[$uGName] = true;
             }
 
-        } 
+        }
         return [
             'access' => [
                 'class' => AccessControl::className(),
@@ -115,30 +116,36 @@ class ScrapController extends Controller
     }
 
 
-
     /**
      * Creates a new Scrap .
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionNew()
+    public function actionNew($work_order_part_id)
     {
         $model = new Scrap();
-
+        $part = WorkOrderPart::find()->where(['id'=>$work_order_part_id])->one();
+        $model->work_order_id = $part->work_order_id;
+        $model->work_order_part_id = $work_order_part_id;
+        $model->part_no = $part->part_no;
+        $model->description = $part->desc;
+        $model->serial_no = $part->serial_no;
+        $model->batch_no = $part->batch_no;
+        $model->created = date('Y-m-d H:i:s');
+        $model->created_by = Yii::$app->user->id;
         if ($model->load(Yii::$app->request->post()) ) {
-
+            $part->status='scrapped';
+            $part->save(false);
             if ($model->save()) {
                 return $this->redirect(['index']);
             }
-        } 
+        }
         return $this->render('new', [
             'model' => $model,
+            'part'=>$part
         ]);
-        
+
     }
-
- 
-
 
     /**
      * Deletes an existing Scrap model by changing the delete status to 1 .
@@ -167,7 +174,7 @@ class ScrapController extends Controller
      * @return mixed
      */
     public function actionDispositionReport($id)
-    {           
+    {
         $this->layout = 'print';
         return $this->render('disposition-report', [
             'model' => $this->findModel($id),
